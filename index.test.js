df7e36ecf249e069aa588c61d4536b56c67ba735
Jorge Anaya <jorge.anaya@unosquare.com>
2020-06-11 19:22:29 -0500

    Fix Test

--- index.test.js
+++ index.test.js
@@ -2,10 +2,10 @@ var isMe = require('./index');
  
 describe('Testing isMe', function(){
     it('should return true passing Jorge', function(){
-        expect(isGeo('Geo')).toBeTruthy();
+        expect(isMe('Jorge')).toBeTruthy();
     });
  
     it('should return false passing Google', function(){
-        expect(isGeo('Google')).toBeFalsy();
+        expect(isMe('Google')).toBeFalsy();
     });
 });
